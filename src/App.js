586b8dbf0e9467913528c7283dae953b8bbcfd5a
Md <848204sarwae@gmail.com>
2025-01-03 21:14:19 +0530

    modified:   package-lock.json

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> hi this a react app made by Danish !!! </p>
         <a
           className="App-link"
           href="https://reactjs.org"
