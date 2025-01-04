120175c2491376bf24209f3b585cbf1cc2aa2cd5
Md <848204sarwae@gmail.com>
2025-01-04 15:15:06 +0530

    Updated the App component and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+//import MyApp from './Component/MyApp'
+import Quiz from './Component/Quiz'
+//import { Component } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p> hi this a react app made by Danish !!! </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Quiz/>
+  </>
+    
   );
 }
 
